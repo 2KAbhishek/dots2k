0088d628625afa4eab7b09f9f06b66c11925efe6
Abhishek Keshri <keshri.abhishek63@gmail.com>
2019-04-08 21:44:20 +0530

    Added option to install requirements with -i or --install

--- setup.sh
+++ setup.sh
@@ -1,9 +1,46 @@
 #!/bin/bash
 
-#Setup script to put files in correct place
+#Setup script for Dotfiles
 CWD=$(pwd)
+arg=$1
 
 echo "Setting up Dotfiles..."
+
+if [[ "$arg" == "-i" || "$arg" == "--install" ]]; then
+	
+    echo "Select an option:"
+    echo "1 for Oh-My-Zsh"
+    echo "2 for Powerlevel9K"
+    echo "3 for Vundle"
+	echo "0 to Exit"
+
+    read option
+
+    case $option in
+
+    "1")echo "Installing Oh-My-Zsh..."
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+        ;;
+
+    "2")echo "Installing Powerlevel9K..."
+        git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+        ;;
+
+    "3")echo "Installing Vundle..."
+        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+        ;;
+
+    "0")echo "Bye"
+        exit 0
+        ;;
+
+    *)echo "Invalid option entered."
+        exit 1
+        ;;
+    esac
+
+    exit 0
+fi
 
 echo "Backing up old files..."
 mv -iv ~/.zshrc ~/.zshrc.old
