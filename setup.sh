eeeb360b6ee803597329052d59047ae5ddf2b818
Abhishek Keshri <keshri.abhishek63@gmail.com>
2019-04-20 05:46:24 +0530

    ANSI Escape Sequences.🎨

--- setup.sh
+++ setup.sh
@@ -4,34 +4,34 @@
 CWD=$(pwd)
 arg=$1
 
-echo "Setting up Dotfiles..."
+echo "\u001b[32;1mSetting up Dotfiles..."
 
 if [[ "$arg" == "-i" || "$arg" == "--install" ]]; then
 
-    echo "Select an option:"
-    echo "1 for Oh-My-Zsh"
-    echo "2 for Powerlevel9K"
-    echo "3 for Vundle"
-    echo "4 for Custom zsh plugins"
-    echo "0 to Exit"
+    echo "\u001b[37;1m\u001b[4mSelect an option:"
+    echo "  \u001b[34;1m1) for Oh-My-Zsh"
+    echo "  \u001b[34;1m2) for Powerlevel9K"
+    echo "  \u001b[34;1m3) for Vundle"
+    echo "  \u001b[34;1m4) for Custom zsh plugins"
+    echo "  \u001b[31;1m0) to Exit"
 
     read option
 
     case $option in
 
-    "1")echo "Installing Oh-My-Zsh..."
+    "1")echo "\u001b[7mInstalling Oh-My-Zsh..."
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
         ;;
 
-    "2")echo "Installing Powerlevel9K..."
+    "2")echo "\u001b[7mInstalling Powerlevel9K..."
         git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
         ;;
 
-    "3")echo "Installing Vundle..."
+    "3")echo "\u001b[7mInstalling Vundle..."
         git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
         ;;
 
-    "4")echo "Installing Custom zsh plugins ..."
+    "4")echo "\u001b[7mInstalling Custom zsh plugins ..."
         cd ~/.oh-my-zsh/custom/plugins
         git clone https://github.com/zsh-users/zsh-autosuggestions
         git clone https://github.com/zdharma/fast-syntax-highlighting.git
@@ -39,11 +39,11 @@ if [[ "$arg" == "-i" || "$arg" == "--install" ]]; then
         cd $CWD
         ;;
 
-    "0")echo "Bye"
+    "0")echo "\u001b[4m\u001b[44mBye"
         exit 0
         ;;
 
-    *)echo "Invalid option entered."
+    *)echo "\u001b[31;1mInvalid option entered."
         exit 1
         ;;
     esac
@@ -51,16 +51,16 @@ if [[ "$arg" == "-i" || "$arg" == "--install" ]]; then
     exit 0
 fi
 
-echo "Backing up old files..."
+echo "\u001b[33;1mBacking up old files..."
 mv -iv ~/.zshrc ~/.zshrc.old
 mv -iv ~/.vimrc ~/.vimrc.old
 mv -iv ~/.tmux.conf ~/.tmux.conf.old
 mv -iv ~/.gitconfig ~/.gitconfig.old
 
-echo "Adding symlinks..."
+echo "\u001b[36;1mAdding symlinks..."
 ln -s $CWD/.zshrc ~/.zshrc
 ln -s $CWD/.vimrc ~/.vimrc
 ln -s $CWD/.tmux.conf ~/.tmux.conf
 ln -s $CWD/.gitconfig ~/.gitconfig
 
-echo "Done."
+echo "\u001b[32;1mDone."
