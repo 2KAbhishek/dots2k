94e092240b392a5f6657c84cb475eebba4e5720c
Abhishek Keshri <keshri.abhishek63@gmail.com>
2019-07-01 00:06:31 +0530

    Added cheat.sh wrapper

---
+++ Scripts/cheat.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Bash Script wrapper for cheat.sh
+topic=$2
+
+case $1 in
+"--color" | "-c")
+    curl cheat.sh/$topic | lolcat
+    ;;
+"--pager" | "-p")
+    curl cheat.sh/$topic | less
+    ;;
+"--help" | "-h")
+    echo "-p for pager\n -c for colorized output"
+    ;;
+*)
+    curl cheat.sh/$topic
+    ;;
+esac
