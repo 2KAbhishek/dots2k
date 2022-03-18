e54de95793c521c46e7b5bbb3840db727e16fa6b
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-03-19 03:20:41 +0530

    Update commit amend alias

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -27,7 +27,7 @@ alias gbrr="git for-each-ref --count=30 --sort=-committerdate refs/heads/ --form
 alias gc="git commit -m"
 alias gcm="git checkout main"
 alias gcma="git commit --amend -m"
-alias gcmn="git commit --amend --no-edit"
+alias gcmn="git add . && git commit --amend --no-edit"
 alias gdh="git diff HEAD"
 alias ghrd="gh repo edit -d 'Sample description'"
 alias ghrh="gh repo edit -h 'https://user.github.io/repo'"
