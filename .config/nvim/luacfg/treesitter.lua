fc1c0ef3c399f67b65448dcc1f8664e225331806
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-21 23:22:20 +0530

    Use all parsers for treesitter

--- .config/nvim/luacfg/treesitter.lua
+++ .config/nvim/luacfg/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-  ensure_installed = "maintained",
+  ensure_installed = "all",
 
   -- Install languages synchronously (only applied to `ensure_installed`)
   sync_install = false,
