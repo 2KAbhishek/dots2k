2b8cb940fad0d0da5ddee1ffcf30429f7ab9560c
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-21 22:28:00 +0530

    Remove empty method

--- .config/nvim/luacfg/lsp-installer.lua
+++ .config/nvim/luacfg/lsp-installer.lua
@@ -2,18 +2,18 @@ local lsp_installer = require("nvim-lsp-installer")
 
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
-lsp_installer.on_server_ready(function(server)
-    local opts = {}
 
+-- lsp_installer.on_server_ready(function(server)
+--     local opts = {}
     -- (optional) Customize the options passed to the server
     -- if server.name == "tsserver" then
     --     opts.root_dir = function() ... end
     -- end
+    -- server:setup(opts)
+-- end)
 
-    -- This setup() function is exactly the same as lspconfig's setup function.
-    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-    server:setup(opts)
-end)
+-- This setup() function is exactly the same as lspconfig's setup function.
+-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 
 lsp_installer.settings({
     ui = {
