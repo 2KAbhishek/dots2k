5abd48f365949e9a9d29e0ba32d62720f306cd50
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-22 00:31:15 +0530

    Add lsp-installer.lua

--- .config/nvim/luacfg/lsp-installer.lua
+++ .config/nvim/luacfg/lsp-installer.lua
@@ -1,26 +1,17 @@
-local lsp_installer = require("nvim-lsp-installer")
+local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+if not status_ok then
+    return
+end
 
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
-
--- lsp_installer.on_server_ready(function(server)
---     local opts = {}
-    -- (optional) Customize the options passed to the server
-    -- if server.name == "tsserver" then
-    --     opts.root_dir = function() ... end
-    -- end
-    -- server:setup(opts)
--- end)
-
--- This setup() function is exactly the same as lspconfig's setup function.
--- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-
-lsp_installer.settings({
-    ui = {
-        icons = {
-            server_installed = "✓",
-            server_pending = "➜",
-            server_uninstalled = "✗"
-        }
+lsp_installer.on_server_ready(function(server)
+    local opts = {
+        on_attach = require("handlers").on_attach,
+        capabilities = require("handlers").capabilities,
     }
-})
+    -- This setup() function is exactly the same as lspconfig's setup function.
+    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+    server:setup(opts)
+end)
+
