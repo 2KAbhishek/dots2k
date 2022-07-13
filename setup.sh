851d357758b6b0bbbc8a5dca0a14c5fc9907c833
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-07-14 03:41:22 +0530

    Add packer for nvim

--- setup.sh
+++ setup.sh
@@ -99,8 +99,8 @@ function install_vim_plugins {
     echo -e "\u001b[7m Installing plugin manager \u001b[0m"
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim \
+        ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
     echo -e "\u001b[7m Installing plugins for vim and nvim... \u001b[0m"
     vim +PlugUpdate +qall
