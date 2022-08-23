669fb721d76d59c7781c92eedcff4c17244c6439
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-08-23 13:40:34 +0530

    Add fzf configs

--- .config/shell/envars.sh
+++ .config/shell/envars.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 # Environment variables
 export EDITOR=nvim
+
+# Fzf
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 export FZF_COMPLETION_TRIGGER=','
+export FZF_DEFAULT_OPTS="
+--layout=reverse
+--info=inline
+--height=80%
+--multi
+--cycle
+--prompt=' ' --pointer=' ' --marker=' '
+--color='hl:148,hl+:154,prompt:blue,pointer:032,marker:010,bg+:000,gutter:000'
+--margin=1
+--border=rounded
+--preview-window=right:65%
+--preview '([[ -f {} ]] && (bat --style=numbers --color=always --line-range=:500 {} || cat {})) || ([[ -d {} ]] && (exa -TFl --group-directories-first --icons --git -L 2 --no-user {} | less)) || echo {} 2> /dev/null | head -200'
+--bind '?:toggle-preview'
+--bind 'ctrl-a:select-all'
+--bind 'ctrl-y:execute-silent(echo {+} | clipcopy)'
+--bind 'ctrl-e:execute(nvim-qt {+})'
+--bind 'ctrl-v:execute(code {+})'
+"
+# export FZF_CTRL_T_COMMAND='fd --type f --hidden --follow --exclude .git'
+export FZF_TMUX_OPTS='-p 90%'
+
 export GREP_COLOR="1;32"
 export ZSH_PLUGINS_ALIAS_TIPS_TEXT="Alias: "
 export ZSH_TMUX_AUTOSTART='false'
