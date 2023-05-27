eae9dc9222dc90f8d6a83641efc1fcdb54abeaed
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-05-27 15:41:40 +0530

    feat: enhance setup function

--- setup.sh
+++ setup.sh
@@ -3,28 +3,123 @@
 # Fetch submodules
 git submodule update --init --recursive
 
+system_kind=""
+
 # Setup script for Dotfiles
-function install_packages {
-    echo -e "\u001b[7m Installing required packages... \u001b[0m"
-    if [ -f /etc/arch-release ]; then
-        sudo pacman -S \
-            curl git zsh python vim neovim tmux bat fzf fasd fd exa unzip \
-            lsd github-cli git-delta lazygit openssh ranger the_silver_searcher \
-            thefuck xclip broot ncdu htop broot xplr ripgrep powerline \
-            kitty libnotify autorandr cmus ttf-firacode-nerd;
+function install_arch {
+    sudo pacman -S \
+        curl git zsh python vim neovim tmux bat fzf fasd fd exa unzip \
+        lsd github-cli git-delta lazygit openssh ranger the_silver_searcher \
+        thefuck xclip broot ncdu htop broot xplr ripgrep powerline \
+        kitty libnotify autorandr cmus ttf-firacode-nerd
+}
+
+function install_debian {
+    sudo apt install \
+        curl git zsh vim neovim tmux bat fzf fasd python exa gh ripgrep htop \
+        silversearcher-ag fd-find unzip thefuck xclip ncdu ranger powerline \
+        kitty libnotify-bin autorandr cmus
+    sudo ln -sfnv /usr/bin/fdfind /usr/bin/fd
+    sudo ln -sfnv /usr/bin/batcat /usr/bin/bat
+}
+
+function install_mac {
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install asdf bat exa fasd fd fzf gh gotop lazygit lsd ncdu neofetch \
+        neovim pastel ripgrep skhd tmux wget yabai
+}
+
+function install_linux {
+    # Identify the Linux distribution
+    if [[ -f "/etc/os-release" ]]; then
+        source /etc/os-release
+        if [[ "$ID" == "arch" ]]; then
+            system_kind="Linux_Arch"
+            install_arch
+        elif [[ "$ID" == "debian" || "$ID_LIKE" == *"debian"* ]]; then
+            system_kind="Linux_Debian"
+            install_debian
+            if [[ "$ID" == "ubuntu" ]]; then
+                system_kind="Linux_Debian_Ubuntu"
+            fi
+            if [[ "$ID" == "linuxmint" ]]; then
+                system_kind="Linux_Debian_Mint"
+            fi
+            if [[ "$ID" == "popos" ]]; then
+                system_kind="Linux_Debian_Pop"
+            fi
+        elif [[ "$ID" == "fedora" ]]; then
+            system_kind="Linux_Fedora"
+        elif [[ "$ID" == "centos" ]]; then
+            system_kind="Linux_CentOS"
+        fi
     else
-        sudo apt install \
-            curl git zsh vim neovim tmux bat fzf fasd python exa gh ripgrep htop \
-            silversearcher-ag fd-find unzip thefuck xclip ncdu ranger powerline \
-            kitty libnotify-bin autorandr cmus;
-        sudo ln -sfnv /usr/bin/fdfind /usr/bin/fd;
-        sudo ln -sfnv /usr/bin/batcat /usr/bin/bat;
+        system_kind="Linux_Unknown"
     fi
+}
+
+function install_packages {
+    if [[ "$(uname)" == "Linux" ]]; then
+        system_kind="Linux"
+        install_linux
+    elif [[ "$(uname)" == "Darwin" ]]; then
+        system_kind="Mac"
+        install_mac
+    # Check if the system is running Windows
+    elif [[ "$(uname -s)" =~ "CYGWIN" ]]; then
+        system_kind="Windows_Cygwin"
+    elif [[ "$(uname -r)" =~ "Microsoft" ]]; then
+        system_kind="Windows_WSL"
+    else
+        system_kind="Unknown"
+    fi
+    echo "Installing packages for $system_kind"
 }
 
 function install_oh_my_zsh {
     echo -e "\u001b[7m Installing oh-my-zsh...\u001b[0m"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+}
+
+function install_zsh_plugins {
+    echo -e "\u001b[7m Installing zsh plugins...\u001b[0m"
+    git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+    git clone https://github.com/marlonrichert/zsh-autocomplete ~/.oh-my-zsh/custom/plugins/zsh-autocomplete
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+    git clone https://github.com/clarketm/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+    git clone https://github.com/z-shell/F-Sy-H.git ~/.oh-my-zsh/custom/plugins/F-Sy-H
+    git clone https://github.com/djui/alias-tips.git ~/.oh-my-zsh/custom/plugins/alias-tips
+    git clone https://github.com/unixorn/git-extra-commands.git ~/.oh-my-zsh/custom/plugins/git-extra-commands
+    git clone https://github.com/Aloxaf/fzf-tab.git ~/.oh-my-zsh/custom/plugins/fzf-tab
+    git clone https://github.com/hlissner/zsh-autopair ~/.oh-my-zsh/custom/plugins/zsh-autopair
+    git clone https://github.com/MichaelAquilina/zsh-auto-notify.git ~/.oh-my-zsh/custom/plugins/auto-notify
+}
+
+function install_vim_plugins {
+    echo -e "\u001b[7m Installing plugin manager \u001b[0m"
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+    echo -e "\u001b[7m Installing plugins for vim and nvim... \u001b[0m"
+    vim +PlugUpdate +qall
+    pip install neovim
+    nvim -c UpdateRemotePlugins
+}
+
+function install_tmux_plugins {
+    echo -e "\u001b[7m Installing tmux plugins... \u001b[0m"
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    tmux start-server
+    tmux new-session -d
+    ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+    tmux kill-server
+}
+
+function install_extras {
+    install_oh_my_zsh
+    install_zsh_plugins
+    install_vim_plugins
+    install_tmux_plugins
 }
 
 function backup_configs {
@@ -106,40 +201,6 @@ function setup_symlinks {
     ln -sfnv "$PWD/.zshrc" ~/
 }
 
-function install_zsh_plugins {
-    echo -e "\u001b[7m Installing zsh plugins...\u001b[0m"
-    git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
-    git clone https://github.com/marlonrichert/zsh-autocomplete ~/.oh-my-zsh/custom/plugins/zsh-autocomplete
-    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-    git clone https://github.com/clarketm/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-    git clone https://github.com/z-shell/F-Sy-H.git ~/.oh-my-zsh/custom/plugins/F-Sy-H
-    git clone https://github.com/djui/alias-tips.git ~/.oh-my-zsh/custom/plugins/alias-tips
-    git clone https://github.com/unixorn/git-extra-commands.git ~/.oh-my-zsh/custom/plugins/git-extra-commands
-    git clone https://github.com/Aloxaf/fzf-tab.git ~/.oh-my-zsh/custom/plugins/fzf-tab
-    git clone https://github.com/hlissner/zsh-autopair ~/.oh-my-zsh/custom/plugins/zsh-autopair
-    git clone https://github.com/MichaelAquilina/zsh-auto-notify.git ~/.oh-my-zsh/custom/plugins/auto-notify
-}
-
-function install_vim_plugins {
-    echo -e "\u001b[7m Installing plugin manager \u001b[0m"
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-    echo -e "\u001b[7m Installing plugins for vim and nvim... \u001b[0m"
-    vim +PlugUpdate +qall
-    pip install neovim
-    nvim -c UpdateRemotePlugins
-}
-
-function install_tmux_plugins {
-    echo -e "\u001b[7m Installing tmux plugins... \u001b[0m"
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    tmux start-server
-    tmux new-session -d
-    ~/.tmux/plugins/tpm/scripts/install_plugins.sh
-    tmux kill-server
-}
-
 function distro_tweaks {
     echo -e "\u001b[7m Distro specific tweaks... \u001b[0m"
     bash "$PWD"/scripts/local_distro.sh
@@ -148,18 +209,26 @@ function distro_tweaks {
 function setup_dotfiles {
     echo -e "\u001b[7m Setting up Dotfiles... \u001b[0m"
     install_packages
-    install_oh_my_zsh
+    install_extras
     backup_configs
     setup_symlinks
-    install_zsh_plugins
-    install_vim_plugins
-    install_tmux_plugins
     distro_tweaks
     echo -e "\u001b[7m Done! \u001b[0m"
 }
 
-if [ "$1" = "--all" -o "$1" = "-a" ]; then
+if [ "$1" = "--all" ] || [ "$1" = "-a" ]; then
     setup_dotfiles
+    exit 0
+fi
+
+if [ "$1" = "--install" ] || [ "$1" = "-i" ]; then
+    install_packages
+    install_extras
+    exit 0
+fi
+
+if [ "$1" = "--symlinks" ] || [ "$1" = "-s" ]; then
+    setup_symlinks
     exit 0
 fi
 
@@ -169,13 +238,10 @@ echo -e "\u001b[32;1m Setting up Dotfiles...\u001b[0m"
 echo -e " \u001b[37;1m\u001b[4mSelect an option:\u001b[0m"
 echo -e "  \u001b[34;1m (0) Setup everything \u001b[0m"
 echo -e "  \u001b[34;1m (1) Install packages \u001b[0m"
-echo -e "  \u001b[34;1m (2) Install oh-my-zsh \u001b[0m"
-echo -e "  \u001b[34;1m (3) Backup current config \u001b[0m"
+echo -e "  \u001b[34;1m (2) Install extras \u001b[0m"
+echo -e "  \u001b[34;1m (3) Backup configs \u001b[0m"
 echo -e "  \u001b[34;1m (4) Setup symlinks \u001b[0m"
-echo -e "  \u001b[34;1m (5) Install zsh plugins \u001b[0m"
-echo -e "  \u001b[34;1m (6) Install vim plugins \u001b[0m"
-echo -e "  \u001b[34;1m (7) Install tmux plugins \u001b[0m"
-echo -e "  \u001b[34;1m (8) Distro specific tweaks \u001b[0m"
+echo -e "  \u001b[34;1m (5) Distro specific tweaks \u001b[0m"
 echo -e "  \u001b[31;1m (*) Anything else to exit \u001b[0m"
 
 echo -en "\u001b[32;1m ==> \u001b[0m"
@@ -193,7 +259,7 @@ case $option in
     ;;
 
 "2")
-    install_oh_my_zsh
+    install_extras
     ;;
 
 "3")
@@ -205,18 +271,6 @@ case $option in
     ;;
 
 "5")
-    install_zsh_plugins
-    ;;
-
-"6")
-    install_vim_plugins
-    ;;
-
-"7")
-    install_tmux_plugins
-    ;;
-
-"8")
     distro_tweaks
     ;;
 
