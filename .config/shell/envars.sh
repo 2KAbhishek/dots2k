260cdb53694dd46d66bd07ca1bd97dad068c068b
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-09-05 01:03:59 +0530

    feat: introduce clipcopy and paste envs

--- .config/shell/envars.sh
+++ .config/shell/envars.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Environment variables
 export EDITOR=nvim
+export CLIPCOPY=wl-copy
+export CLIPPASTE=wl-paste
 
 # Fzf
 export FZF_DEFAULT_COMMAND='fd --hidden --follow --exclude=.git --exclude=node_modules'
