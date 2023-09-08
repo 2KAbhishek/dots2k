758737ab70ea4ea71e8c6d34df7e16ae943d35af
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-09-08 23:13:28 +0530

    refactor: extract out backup configs

--- setup.sh
+++ setup.sh
@@ -123,44 +123,7 @@ function install_extras {
 
 function backup_configs {
     echo -e "\u001b[33;1m Backing up existing files... \u001b[0m"
-    mv -iv ~/.config/autorandr ~/.config/autorandr.old
-    mv -iv ~/.config/awesome/ ~/.config/awesome.old
-    mv -iv ~/.config/bat/config ~/.config/bat/config.old
-    mv -iv ~/.config/broot/conf.toml ~/.config/broot/conf.toml.old
-    mv -iv ~/.config/cmus/darkwind.theme ~/.config/cmus/darkwind.theme.old
-    mv -iv ~/.config/delta ~/.config/delta.old
-    mv -iv ~/.config/fish/ ~/.config/fish.old
-    mv -iv ~/.config/fontconfig/ ~/.config/fontconfig.old
-    mv -iv ~/.config/htop ~/.config/htop.old
-    mv -iv ~/.config/i3 ~/.config/i3.old
-    mv -iv ~/.config/i3status ~/.config/i3status.old
-    mv -iv ~/.config/kitty ~/.config/kitty.old
-    mv -iv ~/.config/libinput-gestures.conf ~/.config/libinput-gestures.conf.old
-    mv -iv ~/.config/nvim ~/.config/nvim.old
-    mv -iv ~/.config/ranger ~/.config/ranger.old
-    mv -iv ~/.config/rofi ~/.config/rofi.old
-    mv -iv ~/.config/shell ~/.config/shell.old
-    mv -iv ~/.config/sysinfo.conkyrc ~/.config/sysinfo.conkyrc.old
-    mv -iv ~/.config/xplr ~/.config/xplr.old
-    mv -iv ~/.asdfrc ~/.asdfrc.old
-    mv -iv ~/.bashrc ~/.bashrc.old
-    mv -iv ~/.bundle ~/.bundle.old
-    mv -iv ~/.dircolors ~/.dircolors.old
-    mv -iv ~/.dmenurc ~/.dmenurc.old
-    mv -iv ~/.gitconfig ~/.gitconfig.old
-    mv -iv ~/.gitignore.global ~/.gitignore.global.old
-    mv -iv ~/.luarc.json ~/.luarc.json.old
-    mv -iv ~/.inputrc ~/.inputrc.old
-    mv -iv ~/.p10k.zsh ~/.p10k.zsh.old
-    mv -iv ~/.prettierrc ~/.prettierrc.old
-    mv -iv ~/.pryrc ~/.pryrc.old
-    mv -iv ~/.pystartup ~/.pystartup.old
-    mv -iv ~/.stylua.toml ~/.stylua.toml.old
-    mv -iv ~/.tmux.conf ~/.tmux.conf.old
-    mv -iv ~/.vimrc ~/.vimrc.old
-    mv -iv ~/.Xresources ~/.Xresources.old
-    mv -iv ~/.zshrc ~/.zshrc.old
-    mv -iv ~/.local/share/konsole/ ~/.local/share/konsole.old
+    bash "$PWD"/scripts/backup_configs.sh
     echo -e "\u001b[36;1m Remove backups with 'rm -ir ~/.*.old && rm -ir ~/.config/*.old'. \u001b[0m"
 }
 
