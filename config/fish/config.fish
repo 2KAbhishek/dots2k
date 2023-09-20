2d7d8b9a917a43b1290c2e508c838267a5941d67
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-09-20 13:05:52 +0530

    feat: add aliases to fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,4 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
     navi widget fish | source
+    cat $HOME/.config/shell/aliases.sh | source
 end
