e524fe7861861494ebe702e9149ca9d77b13db7d
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-10-30 22:12:46 +0530

    feat: remove unused configs

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -6,46 +6,6 @@ from time import localtime, strftime
 
 config.load_autoconfig()
 
-# Cookies
-config.set("content.cookies.accept", "all", "chrome-devtools://*")
-config.set("content.cookies.accept", "all", "devtools://*")
-
-# User agent
-config.set(
-    "content.headers.user_agent",
-    "Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}",
-    "https://web.whatsapp.com/",
-)
-config.set(
-    "content.headers.user_agent",
-    "Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/71.0",
-    "https://accounts.google.com/*",
-)
-config.set(
-    "content.headers.user_agent",
-    "Mozilla/5.0 ({os_info}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99 Safari/537.36",
-    "https://*.slack.com/*",
-)
-config.set(
-    "content.headers.user_agent",
-    "Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/71.0",
-    "https://docs.google.com/*",
-)
-config.set(
-    "content.headers.user_agent",
-    "Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/71.0",
-    "https://drive.google.com/*",
-)
-
-# Permissions
-config.set("content.images", True, "chrome-devtools://*")
-config.set("content.images", True, "devtools://*")
-
-config.set("content.javascript.enabled", True, "chrome-devtools://*")
-config.set("content.javascript.enabled", True, "devtools://*")
-config.set("content.javascript.enabled", True, "chrome://*/*")
-config.set("content.javascript.enabled", True, "qute://*/*")
-
 # Colors
 accent = "#1688f0"
 black = "#000000"
