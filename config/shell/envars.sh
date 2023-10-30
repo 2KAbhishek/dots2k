955111d4b137ccdd43d2a831bd6d2294babf5cae
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-10-31 02:48:58 +0530

    feat: update fzf confs

--- config/shell/envars.sh
+++ config/shell/envars.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Environment variables
 export EDITOR=nvim
+export EDITOR_TERM="$TERMINAL_COMMAND -e $EDITOR"
 export CLIPCOPY=wl-copy
 export CLIPPASTE=wl-paste
 
@@ -11,14 +12,13 @@ export FZF_DEFAULT_OPTS="
 --layout=reverse --info=inline --height=80% --multi --cycle --margin=1 --border=rounded
 --preview '([[ -f {} ]] && (bat --style=numbers --color=always --line-range=:500 {} || cat {})) || ([[ -d {} ]] \
 && (exa -TFl --group-directories-first --icons -L 2 --no-user {} | less)) || echo {} 2> /dev/null | head -200'
---prompt=' ' --pointer='>' --marker='✔'
+--prompt=' ' --pointer=' ' --marker='✔'
 --color='hl:148,hl+:154,prompt:blue,pointer:032,marker:010,bg+:000,gutter:000'
 --preview-window=right:65%
 --bind '?:toggle-preview'
 --bind 'ctrl-a:select-all'
---bind 'ctrl-y:execute-silent(echo {+} | clipcopy)'
---bind 'ctrl-e:execute(nvim-qt {+})'
---bind 'ctrl-v:execute(code {+})'"
+--bind 'ctrl-y:execute-silent(echo {+} | $CLIPCOPY)'
+--bind 'ctrl-e:execute($EDITOR_TERM {+})+reload(fzf)'"
 
 export FZF_CTRL_T_COMMAND='fd -t f -HF -E=.git -E=node_modules'
 export FZF_TMUX_OPTS='-p 90%'
