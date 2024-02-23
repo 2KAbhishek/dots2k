467b06a8f0882ba8e9707333088bfb6a2b561489
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-02-23 08:00:49 +0530

    refactor: organize aliases

--- config/shell/aliases.sh
+++ config/shell/aliases.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Aliases
+
+# Core
 alias ..="cd ../"
 alias ...="cd ../../"
 alias ....="cd ../../../"
@@ -7,45 +8,38 @@ alias .....="cd ../../../../"
 alias 7zc="7z a -mx=9"
 alias acp="advcp -gv"
 alias amv="advmv -gv"
-alias apti="sudo apt install"
-alias apts="apt search"
-alias aptr="sudo apt remove"
-alias aptq="apt show"
-alias aptu="sudo apt update && sudo apt upgrade"
-alias aran="autorandr -l"
-alias asc="asciinema"
-alias blue="bluetuith"
-alias brc="brew cleanup"
-alias bri="brew install"
-alias brl="brew list"
-alias brr="brew uninstall"
-alias bru="brew upgrade"
 alias cat="bat"
-alias ccp="$CLIPCOPY"
 alias cdx='cd $(xplr)'
-alias cless='colorize_less -N'
-alias cols='spectrum_ls'
+alias cpa='"$CLIPPASTE"'
+alias ccp='"$CLIPCOPY"'
 alias cp="cp -irv"
-alias cpa="$CLIPPASTE"
 alias diff="diff --color=auto"
-alias dnfi="sudo dnf install"
-alias dnfs="dnf search"
-alias dnfr="sudo dnf remove"
-alias dnfu="sudo dnf update"
-alias e='nvim $(files)'
-alias emd='emacs --daemon &'
-alias emc="emacsclient -c -a 'emacs'"
-alias emt='emacs --no-window-system'
 alias ez="eza -Ah -s=extension --group-directories-first --icons"
 alias ezl="eza -AhlT -L=1 -s=extension --group-directories-first --icons --git --git-ignore"
 alias ezr="eza -AhlR -L=2 -s=extension --group-directories-first --icons --git --git-ignore"
 alias ezt="eza -ahlT -L=2 -s=extension --group-directories-first --icons --git --git-ignore"
-alias exsu='exercism submit'
 alias fd="fd -H"
 alias fdir="find . -type d -name"
 alias ffil="find . -type f -name"
-alias files="fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'"
-alias fix="fuck"
+alias grep="grep --color=auto"
+alias la="ez"
+alias ll="ezl"
+alias lr="ezr"
+alias lt="ezt"
+alias ldir="ls -d */"
+alias ln="ln -sfnv"
+alias logout="sudo pkill -u \$USER"
+alias ls='ls --color=auto'
+alias lsa="lsd -A --group-dirs first --classify"
+alias lso="lsd -A --group-dirs first --classify --recursive --depth=2"
+alias lsl="lsd -A --group-dirs first --classify -l"
+alias mv="mv -iv"
+alias open="open_command"
+alias rm="rm -irv"
+alias rmf="rm -rf"
+alias q="exit"
+
+# Git
 alias g="git"
 alias ga="git add"
 alias gc="git commit -m"
@@ -73,32 +67,70 @@ alias gmv="git mv"
 alias gmx="git merge -X ours"
 alias gsv="git status -v"
 alias gtop='cd "$(git rev-parse --show-toplevel)"'
-alias grep="grep --color=auto"
+
+# Editor
+alias e='nvim $(files)'
+alias emd='emacs --daemon &'
+alias emc="emacsclient -c -a 'emacs'"
+alias emt='emacs --no-window-system'
+alias vi="nvim"
+alias vimrc="vim ~/.vimrc"
+alias virc="nvim ~/.config/nvim/init.lua"
+alias me='"$EDITOR" README.md'
+alias zshrc='"$EDITOR" ~/.zshrc'
+alias bashrc='"$EDITOR" ~/.bashrc'
+alias alia='"$EDITOR" ~/.config/shell/aliases.sh'
+alias enva='"$EDITOR" ~/.config/shell/envars.sh'
+alias func='"$EDITOR" ~/.config/shell/functions.sh'
+alias loca='"$EDITOR" ~/.local.sh'
+alias p2k='"$EDITOR" ~/.config/shell/powerlevel2k.zsh'
+alias meow='"$EDITOR" ~/.config/kitty/kitty.conf'
+
+# General
+alias asc="asciinema"
+alias cless='colorize_less -N'
+alias cols='spectrum_ls'
+alias exsu='exercism submit'
+alias files="fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'"
+alias fix="fuck"
 alias icat="kitty +kitten icat"
 alias java8="export JAVA_HOME=/usr/lib/jvm/java-8-openjdk/jre"
 alias java11="export JAVA_HOME=/usr/lib/jvm/java-11-openjdk/"
 alias jupn="jupyter notebook"
-alias kpa="keepassxc-cli clip $KEEPASS_DB"
-alias la="ez"
-alias ll="ezl"
-alias lr="ezr"
-alias lt="ezt"
-alias ldir="ls -d */"
-alias litu="sudo light -A 10"
-alias litd="sudo light -U 10"
-alias ln="ln -sfnv"
-alias logout="sudo pkill -u \$USER"
-alias ls='ls --color=auto'
-alias lsa="lsd -A --group-dirs first --classify"
-alias lso="lsd -A --group-dirs first --classify --recursive --depth=2"
-alias lsl="lsd -A --group-dirs first --classify -l"
-alias macres="yabai --restart-service && skhd --restart-service"
-alias mv="mv -iv"
+alias kpa='keepassxc-cli clip "$KEEPASS_DB"'
 alias ncdu="ncdu --color=dark -x"
-alias note='nvim +"Telekasten new_note"'
+alias pick="pastel pick | pastel format hex | ccp"
+alias png="pngquant --ext .png --force"
+alias pp="prettyping --nolegend"
+alias rcop="rubocop --autocorrect"
+alias scr="scrot -s -l width=3,color='#1688f0',mode=edge,opacity=75 -F ~/Pictures/Screenshots/snap-%Y-%m-%d-%H-%M-%S.png -e 'xclip -selection clipboard -t image/png -i ~/Pictures/Screenshots/snap-%Y-%m-%d-%H-%M-%S.png'"
+alias scrf="scrot -cd 3 -F ~/Pictures/Screenshots/snap-%Y-%m-%d-%H-%M-%S.png -e 'xclip -selection clipboard -t image/png -i ~/Pictures/Screenshots/snap-%Y-%m-%d-%H-%M-%S.png'"
+alias scrw="scrot -ubcd 3 -F ~/Pictures/Screenshots/snap-%Y-%m-%d-%H-%M-%S.png -e 'xclip -selection clipboard -t image/png -i ~/Pictures/Screenshots/snap-%Y-%m-%d-%H-%M-%S.png'"
+alias serv="npx live-server --port=1234 ."
+alias sto="stackoverflow"
+alias tmux="tmux -u"
+alias tmuxm="tmux new-session \; split-window -h \; split-window -v \; attach"
+alias wit="wiki-tui"
+alias xp="xplr"
+alias ytdl="youtube-dl"
+
+# Package Manger
+alias apti="sudo apt install"
+alias apts="apt search"
+alias aptr="sudo apt remove"
+alias aptq="apt show"
+alias aptu="sudo apt update && sudo apt upgrade"
+alias brc="brew cleanup"
+alias bri="brew install"
+alias brl="brew list"
+alias brr="brew uninstall"
+alias bru="brew upgrade"
+alias dnfi="sudo dnf install"
+alias dnfs="dnf search"
+alias dnfr="sudo dnf remove"
+alias dnfu="sudo dnf update"
 alias npi="npm install"
 alias npr="npm run"
-alias open="open_command"
 alias paci="sudo pacman -S"
 alias pacq="pacman -Qi"
 alias pacr="sudo pacman -Rsc"
@@ -107,57 +139,32 @@ alias pacs="pacman -Ss"
 alias pacl="pacman -Q"
 alias pacm="sudo pacman-mirrors --fasttrack 5 && sudo pacman -Syu"
 alias pacu="sudo pacman -Syu"
-alias pick="pastel pick | pastel format hex | ccp"
+alias trii="trizen -S --noedit"
+alias tris="trizen -Ss --noedit"
+alias triu="trizen -Syu --noedit"
+alias yayi="yay -S"
+alias yayl="pacman -Qm"
+alias yayu="yay -Syu"
+
+# System
+alias aran="autorandr -l"
+alias blue="bluetuith"
+alias litu="sudo light -A 10"
+alias litd="sudo light -U 10"
+alias macres="yabai --restart-service && skhd --restart-service"
 alias plasmar="kquitapp5 plasmashell && kstart5 plasmashell"
-alias png="pngquant --ext .png --force"
-alias pp="prettyping --nolegend"
-alias q="exit"
-alias rcop="rubocop --autocorrect"
-alias rm="rm -irv"
-alias rmf="rm -rf"
-# alias serv="python -m http.server 1234"
-alias scr="scrot -s -l width=3,color='#1688f0',mode=edge,opacity=75 -F ~/Pictures/Screenshots/snap-%Y-%m-%d-%H-%M-%S.png -e 'xclip -selection clipboard -t image/png -i ~/Pictures/Screenshots/snap-%Y-%m-%d-%H-%M-%S.png'"
-alias scrf="scrot -cd 3 -F ~/Pictures/Screenshots/snap-%Y-%m-%d-%H-%M-%S.png -e 'xclip -selection clipboard -t image/png -i ~/Pictures/Screenshots/snap-%Y-%m-%d-%H-%M-%S.png'"
-alias scrw="scrot -ubcd 3 -F ~/Pictures/Screenshots/snap-%Y-%m-%d-%H-%M-%S.png -e 'xclip -selection clipboard -t image/png -i ~/Pictures/Screenshots/snap-%Y-%m-%d-%H-%M-%S.png'"
-alias serv="npx live-server --port=1234 ."
 alias shad="ssh-add ~/.ssh/id_rsa"
 alias shag='eval "$(ssh-agent -s)"'
-alias sto="stackoverflow"
 alias sysd="sudo systemctl disable"
 alias syse="sudo systemctl enable"
 alias sysr="sudo systemctl restart"
 alias syss="systemctl status"
 alias systa="sudo systemctl start"
 alias systo="sudo systemctl stop"
-alias tmux="tmux -u"
-alias tmuxm="tmux new-session \; split-window -h \; split-window -v \; attach"
-alias trii="trizen -S --noedit"
-alias tris="trizen -Ss --noedit"
-alias triu="trizen -Syu --noedit"
 alias topg="topgrade --no-retry -y"
 alias topG="topgrade --no-retry -y --only git_repos"
-alias vi="nvim"
 alias vol="pulsemixer"
 alias volu="amixer sset 'Master' 10%+"
 alias vold="amixer sset 'Master' 10%-"
 alias wifi="nmtui"
-alias wit="wiki-tui"
-alias xp="xplr"
-alias yayi="yay -S"
-alias yayl="pacman -Qm"
-alias yayu="yay -Syu"
-alias ytdl="youtube-dl"
-
-alias vimrc="vim ~/.vimrc"
-alias virc="nvim ~/.config/nvim/init.lua"
-alias me="$EDITOR README.md"
-alias zshrc="$EDITOR ~/.zshrc"
-alias bashrc="$EDITOR ~/.bashrc"
-alias alia="$EDITOR ~/.config/shell/aliases.sh"
-alias enva="$EDITOR ~/.config/shell/envars.sh"
-alias func="$EDITOR ~/.config/shell/functions.sh"
-alias loca="$EDITOR ~/.local.sh"
-alias p2k="$EDITOR ~/.config/shell/powerlevel2k.zsh"
-
-alias meow="$EDITOR ~/.config/kitty/kitty.conf"
 
