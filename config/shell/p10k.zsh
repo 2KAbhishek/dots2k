6bd91017d78bac2a9ac26ca74d504e199866343f
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-04-07 12:09:21 +0530

    feat: black folder bg color

--- config/shell/p10k.zsh
+++ config/shell/p10k.zsh
@@ -220,7 +220,7 @@
   # Replace removed segment suffixes with this symbol.
   typeset -g POWERLEVEL9K_SHORTEN_DELIMITER=
   # Color of the shortened directory segments.
-  typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=250
+  typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=0
   # Color of the anchor directory segments. Anchor segments are never shortened. The first
   # segment is always an anchor.
   typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND=255
