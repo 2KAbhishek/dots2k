3a0f8b95251c624340bc797b1550d09c95bc4837
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-05-20 03:20:53 +0530

    feat: add aliases.zsh

---
+++ config/zsh/aliases.zsh
@@ -0,0 +1,17 @@
+# zsh Aliases
+alias reload="source $ZDOTDIR/.zshrc"
+
+# Suffix aliases
+alias -s md=nvim
+alias -s html=nvim
+
+# Global aliases
+alias -g C="| $CLIPCOPY"
+alias -g F="| fpp -ko -nfc"
+alias -g G="| grep"
+alias -g L="| wc -l"
+alias -g Q="&& exit"
+alias -g Z="| fzf"
+alias -g wcc="| wc -m"
+alias -g wcw="| wc -w"
+
