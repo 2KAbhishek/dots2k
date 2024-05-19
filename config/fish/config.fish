5a74a407b35e79127ba8c63958f80374395a8154
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-05-20 04:06:46 +0530

    feat: add fzf to fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,8 +1,7 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    fzf --fish | source
     zoxide init fish | source
     navi widget fish | source
     mise activate fish | source
-    mise completion fish | source
     cat $HOME/.config/shell/aliases.sh | source
 end
