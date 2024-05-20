056d51581e8c4dc9f9b0f92a72acd744f41693b2
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-05-20 11:58:57 +0530

    feat: add auto accept keybinding

--- config/zsh/keys.zsh
+++ config/zsh/keys.zsh
@@ -9,6 +9,7 @@ zle -N copy-earlier-word
 bindkey -s '^H' ' reload^M ^M'
 bindkey -s '^T' ' t^M ^M'
 bindkey -s "^G" ' lazygit^M ^M'
+bindkey '^K' autosuggest-accept
 bindkey "^F" fzf-file-widget
 bindkey "^X^E" edit-command-line
 bindkey "^[." insert-last-word
