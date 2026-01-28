1315b5eb2f4f8cc41fab4d00de9af501889955c4
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-01-28 09:42:46 +0530

    fix: color escape function

--- config/shell/functions.sh
+++ config/shell/functions.sh
@@ -147,27 +147,20 @@ color_text() {
 color_escape() {
     local fgc bgc vals seq0
 
-    printf "Color escapes are %s\n" '\e[${value};...;${value}m'
+    printf "Color escapes are %%s\n" "\\e[\${value};...;\${value}m"
     printf "Values 30..37 are \e[33mforeground colors\e[m\n"
     printf "Values 40..47 are \e[43mbackground colors\e[m\n"
     printf "Value  1 gives a  \e[1mbold-faced look\e[m\n\n"
 
-    # foreground colors
     for fgc in {30..37}; do
-        # background colors
         for bgc in {40..47}; do
-            fgc=${fgc#37} # white
-            bgc=${bgc#40} # black
+            vals="${fgc};${bgc}"
+            seq0="\e[${vals}m"
 
-            vals="${fgc:+$fgc;}${bgc}"
-            vals=${vals%%;}
-
-            seq0="${vals:+\e[${vals}m}"
-            printf "  %-9s" "${seq0:-(default)}"
+            printf "  %-9s" "${vals}"
             printf " ${seq0}TEXT\e[m"
-            printf " \e[${vals:+${vals+$vals;}}1mBOLD\e[m"
+            printf " \e[${vals};1mBOLD\e[m"
         done
-        echo
         echo
     done
 }
