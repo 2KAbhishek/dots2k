dc237281ddfbf41e650b798a595f59bba28c7871
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-01-28 09:43:20 +0530

    feat: add fancy-ctrl-z func

--- config/shell/functions.sh
+++ config/shell/functions.sh
@@ -164,3 +164,14 @@ color_escape() {
         echo
     done
 }
+
+fancy-ctrl-z() {
+    if [[ $#BUFFER -eq 0 ]]; then
+        BUFFER="fg"
+        zle accept-line -w
+    else
+        zle push-input -w
+        zle clear-screen -w
+    fi
+}
+
