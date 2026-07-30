67d0bf3e8fa7511cd346207ccfd5e0b4d4a95d41
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-07-30 20:17:04 +0530

    feat: add new global aliases

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -6,16 +6,27 @@ which='(alias; declare -f) | /usr/bin/which --tty-only --read-alias --read-funct
 alias -s md=nvim
 alias -s html=nvim
 
-# Global aliases
-alias -g C="| $CLIPCOPY"
+# Global aliases (Robust & Safe)
+alias -g A1='| awk "{print \$1}"'
+alias -g A2='| awk "{print \$2}"'
+alias -g COL="| column -t"
+alias -g C='| $CLIPCOPY'
+alias -g DN=">/dev/null 2>&1"
 alias -g F="| fzf"
+alias -g FP="| fzf --preview 'bat --color=always --style=numbers --line-range :300 {}'"
 alias -g G="| grep"
 alias -g H="| head -30"
+alias -g J="| jq -C"
+alias -g JX="| fx"
 alias -g L="| wc -l"
+alias -g NE="2>/dev/null"
 alias -g Q="&& exit"
+alias -g US="| sort | uniq -c | sort -rn"
 alias -g T="| tail -30"
+alias -g X="| xargs"
 
 alias -g fgs="| fzf --preview 'git show --color=always {1}'"
 alias -g wcc="| wc -m"
 alias -g wcw="| wc -w"
-alias -g share="| curl -F 'file=@-' 0x0.st"
+alias -g shareit='| curl -L -F "file=@-" https://0x0.st | $CLIPCOPY'
+
